d24e339a0665d2128bce5af7f28bf942f33d0cb0
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2024-04-18 18:22:41 +0330

    Ch01-L04-Components

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>main</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

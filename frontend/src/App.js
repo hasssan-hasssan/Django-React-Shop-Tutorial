a82ba46ed952af3b9e3adebd9028373c8edf17e6
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2024-04-18 23:53:25 +0330

    Ch01-L05-ReactBootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h1>main</h1>
+        <Container>
+          <h1 className='py-5'>main</h1>
+        </Container>
       </main>
       <Footer />
     </div>

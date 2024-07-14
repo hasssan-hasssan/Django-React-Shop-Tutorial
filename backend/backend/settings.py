444619c8f8dfeb3d0999e2b19e67f683f7adbd08
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2024-07-14 16:12:27 +0330

    Ch02-L03-JsonResponse

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,12 +35,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My apps
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [

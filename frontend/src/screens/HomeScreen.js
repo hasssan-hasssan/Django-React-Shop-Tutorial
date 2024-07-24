88878d077b1c5782f90ecf4e4d821448fdab8380
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2024-07-24 07:14:58 +0330

    Ch02-L07-ReactAxios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../products'
 import Product from '../components/Product'
+import axios from 'axios'
 
 function HomeScreen() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+
+        async function fetchProducts() {
+            const { data } = await axios.get('http://127.0.0.1:8000/api/v1/products/')
+            setProducts(data)
+        }
+
+        fetchProducts()
+    }, [])
+
+
     return (
         <div>
             <h1 className='py-4'>Latest Products</h1>

e93a2d7d302cdfb6aaa1e74365476f49e23aab1a
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2024-07-24 19:58:51 +0330

    Ch02-L09-DjangoCors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,12 +45,15 @@ INSTALLED_APPS = [
     
     # 3rd-party apps
     'rest_framework',
+    'corsheaders',
     
     # My apps
     'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +135,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Cors settings
+CORS_ALLOW_ALL_ORIGINS = True

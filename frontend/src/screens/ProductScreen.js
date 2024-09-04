fe522c920bf51fe43d294116703b65ded35b193d
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2024-09-04 21:08:51 +0330

    Ch04-L01-Qty

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, Button, ListGroup } from 'react-bootstrap'
+import { Row, Col, Image, Button, ListGroup, ButtonGroup, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchProductDetails } from '../redux/features/Product/productThunk'
@@ -12,7 +12,11 @@ function ProductScreen() {
     const dispatch = useDispatch()
     let { id } = useParams()
     const { loading, error, product } = useSelector(state => state.productDetails)
+    const [qty, setQty] = useState(1)
 
+    const updateQty = (value) => {
+        setQty(prevState => prevState + value)
+    }
 
     useEffect(() => {
         dispatch(fetchProductDetails(id))
@@ -53,6 +57,32 @@ function ProductScreen() {
                                                 <Col>{product.countInStock > 0 ? "In Stock" : "Out of Stock"}</Col>
                                             </Row>
                                         </ListGroup.Item>
+
+                                        {product.countInStock > 0 && (
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Qty:</Col>
+                                                    <Col>
+                                                        <ButtonGroup size='sm'>
+                                                            <Button
+                                                                disabled={qty === 1}
+                                                                onClick={() => updateQty(-1)}
+                                                                size='sm'>
+                                                                <i class="fa-solid fa-minus"></i>
+                                                            </Button>
+                                                            <Form.Control value={qty} size='sm' readOnly />
+                                                            <Button
+                                                                disabled={qty === product.countInStock}
+                                                                onClick={() => updateQty(+1)}
+                                                                size='sm'>
+                                                                <i class="fa-solid fa-plus"></i>
+                                                            </Button>
+                                                        </ButtonGroup>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                        )}
+
                                         <ListGroup.Item>
                                             <Button className='w-100' disabled={product.countInStock === 0}>add to cart</Button>
                                         </ListGroup.Item>

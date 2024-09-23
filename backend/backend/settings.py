61a1e6c258deefc2e9f9c0c06b50c5015e9da048
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2024-09-23 16:59:02 +0330

    Ch05-L04-Implement-jwt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os 
+import os
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -42,18 +42,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # 3rd-party apps
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
-    
+
     # My apps
     'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,3 +149,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cors settings
 CORS_ALLOW_ALL_ORIGINS = True
+
+
+# Djangorestframework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
 urlpatterns = [
     path('', views.getRoutes, name='getRoutes'),
     path('products/', views.getProducts, name='getProducts'),
-    path('products/<str:pk>/', views.getProduct, name='getProduct')
+    path('products/<str:pk>/', views.getProduct, name='getProduct'),
+
+    path('users/login/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token-refresh')
 ]

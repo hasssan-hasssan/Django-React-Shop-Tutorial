5527c08b2a0aeedae41234780597266acd09f848
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2025-02-13 23:43:25 +0330

    Ch08-L05-Create-TransactionModel

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Product(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -10,12 +11,13 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, db_default=0)
-    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
-    price = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=3, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=9, decimal_places=2, null=True, blank=True)
     countInStock = models.IntegerField(null=True, blank=True, db_default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
-    
-    
+
     def __str__(self):
         return str(self.name)
 
@@ -37,13 +39,17 @@ class Order(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
-    taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    totalPrice = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    taxPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    shippingPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    totalPrice = models.DecimalField(
+        max_digits=9, decimal_places=2, null=True, blank=True)
     isPaid = models.BooleanField(db_default=False)
     paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     isDelivered = models.BooleanField(db_default=False)
-    deliveredAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    deliveredAt = models.DateTimeField(
+        auto_now_add=False, null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -56,7 +62,8 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     qty = models.IntegerField(null=True, blank=True, db_default=0)
-    price = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=9, decimal_places=2, null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
@@ -65,12 +72,42 @@ class OrderItem(models.Model):
 
 class ShippingAddress(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
-    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    order = models.OneToOneField(
+        Order, on_delete=models.CASCADE, null=True, blank=True)
     address = models.CharField(max_length=200, null=True, blank=True)
     city = models.CharField(max_length=200, null=True, blank=True)
     postalCode = models.CharField(max_length=200, null=True, blank=True)
     country = models.CharField(max_length=200, null=True, blank=True)
-    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    
+    shippingPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+
     def __str__(self):
         return str(self.address)
+
+
+class Zibal(models.Model):
+    class Meta:
+        verbose_name_plural = 'Zibal Payments'
+
+    _id = models.AutoField(primary_key=True, editable=False)
+    trackId = models.IntegerField(unique=True)
+    lastStatus = models.SmallIntegerField(null=True, blank=True)
+    refNumber = models.BigIntegerField(null=True, blank=True)
+    amountCreated = models.IntegerField(null=True, blank=True)
+    amountPaid = models.IntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    cardNumber = models.CharField(max_length=16, null=True, blank=True)
+    createdAt_Z = models.DateTimeField(null=True, blank=True)
+    verifiedAt = models.DateTimeField(null=True, blank=True)
+    paidAt = models.DateTimeField(null=True, blank=True)
+    createdAt = models.DateTimeField(null=True, blank=True)
+    updatedAt = models.DateTimeField(null=True, blank=True)
+    # multiplexingInfos = models.JSONField(null=True, blank=True)
+    # wage = models.SmallIntegerField(null=True, blank=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.trackId)

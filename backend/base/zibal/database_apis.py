98b0aec2a2c8e073461afe4565db01589700a95a
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2025-03-04 00:27:42 +0330

    Ch08-L10-CreateMethods

--- backend/base/zibal/database_apis.py
+++ backend/base/zibal/database_apis.py
@@ -1,2 +1,24 @@
+import logging
+from django.contrib.auth.models import User
+from base.models import Order, Zibal
+from base.strConst import (
+    TRACK_ID, ERROR_TRANSACTION_CREATE_DB, MORE_DETAILS
+)
+
+
 class ZibalDatabaseAPIs:
-    pass
+
+    def create(self, order: Order, user: User, data: dict) -> bool:
+        try:
+            Zibal.objects.create(
+                trackId=data.get(TRACK_ID),
+                lastStatus=0,
+                amountCreated=int(order.totalPrice * 10),
+                order=order,
+                user=user
+            )
+            return True
+        except Exception as e:
+            logging.error(ERROR_TRANSACTION_CREATE_DB +
+                          MORE_DETAILS % {'e': e})
+            return False
--- backend/base/zibal/server_apis.py
+++ backend/base/zibal/server_apis.py
@@ -1,2 +1,41 @@
+import requests
+from django.conf import settings
+from base.models import Order
+from base.strConst import (
+    UNKNOWN_ERROR, MERCHANT, AMOUNT, ORDER_ID, CALLBACK_URL, ZIBAL_DOMAIN_IPG, REQUEST_PATH
+)
+
+
 class ZibalServerAPIs:
-    pass
+
+    def __init__(self):
+        self.merchant = settings.ZIBAL_MERCHANT
+
+    def request(self, order: Order) -> dict:
+        data = {}
+        data[MERCHANT] = self.merchant
+        data[AMOUNT] = int(order.totalPrice * 10)
+        data[ORDER_ID] = str(order._id)
+        data[CALLBACK_URL] = 'http://localhost:8000/callbackUrl'
+        return self.postTo(REQUEST_PATH, data)
+
+    def result_code_translator(self, result: int) -> str:
+        code_translator = {
+            100: "Request completed successfully",
+            102: "Merchant not found.",
+            103: "Merchant is inactive.",
+            104: "Invalid merchant.",
+            105: "Amount must be greater than 1,000 Rials.",
+            106: "Invalid callback URL. (Must start with http or https)",
+            113: "Transaction amount exceeds the maximum limit.",
+            114: "Invalid national code.",
+            201: "Previously confirmed.",
+            202: "Order has not been paid or was unsuccessful. Please refer to the status table for more information.",
+            203: "Invalid track ID.",
+        }
+        return code_translator.get(result, UNKNOWN_ERROR)
+
+    def postTo(self, path: str, parameters: dict) -> dict:
+        url: str = ZIBAL_DOMAIN_IPG + path
+        response = requests.post(url, json=parameters)
+        return response.json()

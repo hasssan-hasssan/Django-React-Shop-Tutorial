9e124946c27d8cc513f5d6fdc1acd3bb74b0e1e8
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2025-03-13 20:59:39 +0330

    Ch08-L18-End-three-main-steps

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -2,10 +2,22 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(PaymentToken)
+@admin.register(Zibal)
+class ZibalAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'trackId', 'lastStatus', 'refNumber', 'amountCreated',
+        'amountPaid', 'description', 'cardNumber', 'createdAt_Z',
+        'verifiedAt', 'paidAt', 'createdAt', 'updatedAt', 'order', 'user'
+    )
+
+
+@admin.register(PaymentToken)
+class PaymentTokenAdmin(admin.ModelAdmin):
+    readonly_fields = ('token', 'trackId', 'orderId')
+
+
 admin.site.register(Product)
 admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
-admin.site.register(Zibal)

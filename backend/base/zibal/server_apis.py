892e8c8f3d7f29f4f00d8ee64f44d4852273ec6c
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2025-03-16 03:32:04 +0330

    Ch08-L22-Complete-ZibalServerAPIs-methods

--- backend/base/zibal/server_apis.py
+++ backend/base/zibal/server_apis.py
@@ -1,11 +1,11 @@
 import requests
 from django.urls import reverse
 from django.conf import settings
-from base.models import Order
+from base.models import Order, Zibal
 from base.strConst import (
-    UNKNOWN_ERROR, MERCHANT, AMOUNT, ORDER_ID, 
+    UNKNOWN_ERROR, MERCHANT, AMOUNT, ORDER_ID,
     CALLBACK_URL, ZIBAL_DOMAIN_IPG, REQUEST_PATH,
-    TRACK_ID, VERIFY_PATH
+    TRACK_ID, VERIFY_PATH, INQUIRY_PATH, SUCCESS, MSG, STATUS, REF_NO
 )
 
 
@@ -27,6 +27,46 @@ class ZibalServerAPIs:
         data[MERCHANT] = self.merchant
         data[TRACK_ID] = trackId
         return self.postTo(VERIFY_PATH, data)
+
+    def inquiry(self, trackId: int) -> dict:
+        data = {}
+        data[MERCHANT] = self.merchant
+        data[TRACK_ID] = trackId
+        return self.postTo(INQUIRY_PATH, data)
+
+    def generate_inquiry_pay_response(self, transaction: Zibal) -> dict:
+        response = {}
+        response[SUCCESS] = transaction.lastStatus in [1]
+        response[MSG] = self.payment_status_code_translator(
+            transaction.lastStatus)
+        response[TRACK_ID] = transaction.trackId
+        response[ORDER_ID] = transaction.order._id
+        response[STATUS] = transaction.lastStatus
+        response[REF_NO] = transaction.refNumber if transaction.refNumber else "N/A"
+        return response
+
+    def payment_status_code_translator(self, status: int) -> str:
+        code_translator = {
+            -1: "Pending payment",
+            -2: "Internal error",
+            1: "Paid - Confirmed",
+            2: "Paid - Unconfirmed",
+            3: "Canceled by user",
+            4: "Invalid card number",
+            5: "Insufficient account balance",
+            6: "Incorrect password",
+            7: "Request limit exceeded",
+            8: "Daily online payment limit exceeded",
+            9: "Daily online payment amount exceeded",
+            10: "Invalid card issuer",
+            11: "Switch error",
+            12: "Card not accessible",
+            15: "Transaction refunded",
+            16: "Transaction being refunded",
+            18: "Transaction reversed",
+        }
+        
+        return code_translator.get(status, UNKNOWN_ERROR)
 
     def result_code_translator(self, result: int) -> str:
         code_translator = {
